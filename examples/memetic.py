001d7819052c6e73190e17e0fa8fe842278cd4e8
RincewindWizzard <RincewindWizzard@0xdf.de>
2015-11-28 17:19:30 +0100

    funktional

--- examples/memetic.py
+++ examples/memetic.py
@@ -4,33 +4,44 @@ import random, copy, operator
 from dotgraph import graph, showGraph
 from sgp import *
 
-POOLSIZE = 100
-MAX_ITER = 100
 
-def SGP_solver(groups, size, weeks):
+def SGP_solver(groups, size, weeks, poolsize = 100, max_iter = 100):
+  tabu   = [ [] for w in range(weeks) ]
   pool = []
-  for i in range(POOLSIZE):
+
+  for i in range(poolsize):
     solution = generateSolution(groups, size, weeks)
     pool.append((violations(solution), solution))
+    
 
-  for j in range(MAX_ITER):
+  for iteration in range(max_iter):
     violats, solution = copy.deepcopy(select(pool))
-    mutate(solution)
-    learning(solution)
+
+    enhance(solution, iteration, tabu)
+
+    # schlechteste Lösung rausschmeißen
     max_index, max_value = max(enumerate(pool), key=operator.itemgetter(1))
     pool[max_index] = (violations(solution), solution)
     if pool[max_index][0] == 0: # es wurde eine korrekte Lösung gefunden
       break
 
-  return min(pool)
+  violats, solution = min(pool)
+  return solution, violats
+
+# hier wird die lokale Tabu suche angewendet
+def learning(solution, iteration, tabu):
+  ...
 
 # wählt eine Lösung aus dem Pool aus
 def select(pool):
   return random.choice(pool)
 
-# hier wird die lokale Tabu suche angewendet
-def learning(solution):
-  ...
+# hier wird eine gegebene Lösung verbessert
+def enhance(solution, iteration, tabu):
+  mutate(solution)
+  learning(solution, iteration, tabu)
+  
+
 
 if __name__ == '__main__':
   if len(sys.argv) == 4:
@@ -42,6 +53,7 @@ if __name__ == '__main__':
     size = 4
     weeks = 5
 
-  violats, solution = SGP_solver(groups, size, weeks)
+  solution, violats = SGP_solver(groups, size, weeks)
+  
   print(repr_solution(solution), violats)
   #showGraph(solution)
